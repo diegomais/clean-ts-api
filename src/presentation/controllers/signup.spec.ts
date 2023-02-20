b0c1016cfa93ed04fdc8caeff4b053ad980e1751
Diego Mais <diegomais.dev@gmail.com>
2023-02-20 20:20:38 -0300

    test(SignUpController): ensure returns statusCode 400 and an error if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -14,4 +14,18 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing parameter: name'))
   })
+
+  it('should return statusCode 400 and an error if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name@example.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing parameter: email'))
+  })
 })
