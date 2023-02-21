66e84261fe614668ee85b56bf2a5bd17efb4a8e9
Diego Mais <diegomais.dev@gmail.com>
2023-02-20 21:04:49 -0300

    refactor(SignUpController): use for loop to remove duplicated code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,17 +1,13 @@
 export class SignUpController {
   handle (httpRequest: any): any {
-    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-    if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new Error('Missing parameter: email')
-      }
-    }
-    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-    if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new Error('Missing parameter: name')
+    const requiredFields = ['name', 'email']
+    for (const field of requiredFields) {
+      // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+      if (!httpRequest.body[field]) {
+        return {
+          statusCode: 400,
+          body: new Error(`Missing parameter: ${field}`)
+        }
       }
     }
   }
