9d73fb42db0bc56a13acec86aa35861f60c9fa6f
Diego Mais <diegomais.dev@gmail.com>
2023-04-13 20:08:22 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/db-add-account.spec.ts
+++ src/data/usecases/db-add-account.spec.ts
@@ -92,4 +92,20 @@ describe('DbAddAccount Use-Case', () => {
     const promise = sut.execute(accountData)
     await expect(promise).rejects.toThrow()
   })
+
+  it('should return an account on success', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const account = await sut.execute(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password'
+    })
+  })
 })
